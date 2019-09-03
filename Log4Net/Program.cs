c7a6309ae0856e72834c72f5920d03cc38a98506
AksalBilal <bilal.aksal@gmail.com>
2019-09-03 17:27:49 +0300

    Log4net-Basit-Ornek

---
+++ Log4Net/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4Net
+
+{
+    class Program
+    {
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+                (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+           //proje içindeki bin/debug klasörü altında log.txt dosyasına kayıt edilmektedir.
+            Console.WriteLine("Writing to \"log.txt\" in the same directory as the .exe file.\n");
+            log.Info("Info logging");//İnfo loglanması
+            log.Fatal("Info Fatal");    
+            log.Warn("Info Warning");
+            try
+            {
+                throw new Exception("Exception!");
+            }
+            catch (Exception e)
+            {
+                log.Error("This is my error", e);//Error loglanması
+            }
+            Console.WriteLine("[any key to exit]");
+            Console.ReadKey();
+        }
+    }
+}
